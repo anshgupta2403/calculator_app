ab7d4717c456a4d5e0efb654042ffe93fbb5bfe2
ansh-gupta <58004288+anshgupta2403@users.noreply.github.com>
2025-07-15 12:59:33 +0530

    fix: update the package name and set minSdk to 24

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.calculator_app"
+    namespace = "com.ansh.calculator"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -30,10 +30,10 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.calculator_app"
+        applicationId = "com.ansh.calculator"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = 24
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- android/app/src/main/kotlin/com/ansh/calculator/MainActivity.kt
+++ android/app/src/main/kotlin/com/ansh/calculator/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.calculator_app
+package com.ansh.calculator
 
 import io.flutter.embedding.android.FlutterActivity
 
